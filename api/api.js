49da6462d459c088cd011930ef932c8b17c1b272
Kalyo-Airan-da-Silva <kalyosilva@gmail.com>
2026-06-24 15:57:42 -0300

    Criação da Pipeline CI

--- api/api.js
+++ api/api.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-const fs = require('node:fs')
-
 app.get('/', (req, res) =>{
     res.status(200).json({
         "titulo" : "Easy Delivery API",
